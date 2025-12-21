1e46ec0d70dd814068bed6a6d78a20e2307bfe89
Sigit Prasetyo <75189577+Errs404@users.noreply.github.com>
2025-12-22 03:02:02 +0700

    Update product and user pages for consistency in title and SEO metadata

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="light">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +14,7 @@
     <link rel="icon" type="image/png" href="{{asset('assets/logo.png') }}">
     
     <!-- PWA Manifest -->
-    <<link rel="manifest" href="{{ asset('assets/manifest-DTaoG9pG.json') }}">
+    <link rel="manifest" href="{{ asset('assets/manifest-DTaoG9pG.json') }}">
 
     <!-- Preload critical fonts -->
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" as="style">
